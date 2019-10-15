2858c54d1773133713ab344f41d92c2c6f5df841
Reznot <Reznot24@gmail.com>
2019-10-15 14:21:21 +0200

    router to note activity

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnUnlockNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnUnlockNote = (Button) findViewById(R.id.unlock_button);
+
+        btnUnlockNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+    }
+
+    private void launchActivity() {
+        Intent intent = new Intent(this, note_screen.class);
+        startActivity(intent);
     }
 }
