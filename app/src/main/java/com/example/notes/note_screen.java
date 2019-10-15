f6ac97df841bece333c26098061e4b281e686292
Reznot <Reznot24@gmail.com>
2019-10-15 14:50:52 +0200

    basic screens and routing

--- app/src/main/java/com/example/notes/note_screen.java
+++ app/src/main/java/com/example/notes/note_screen.java
@@ -2,13 +2,31 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class note_screen extends AppCompatActivity {
 
+    private Button btnChangePassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_screen);
+
+        btnChangePassword = (Button) findViewById(R.id.changePasswordBtn);
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+    }
+
+    private void launchActivity() {
+        Intent intent = new Intent(this, PasswordChangeActivity.class);
+        startActivity(intent);
     }
 }
