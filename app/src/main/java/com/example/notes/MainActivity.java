d17601bb5dae295e87c0647c03e1de3c8c486b70
Reznot <Reznot24@gmail.com>
2019-10-15 15:48:22 +0200

    note stored in private storage

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button btnUnlockNote;
-    static String noteText = "Siemanko to moja super notatka";
+    static String noteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/notes/note_screen.java
+++ app/src/main/java/com/example/notes/note_screen.java
@@ -2,11 +2,17 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.HashSet;
+
+import static com.example.notes.MainActivity.noteText;
 
 public class note_screen extends AppCompatActivity {
 
@@ -20,13 +26,22 @@ public class note_screen extends AppCompatActivity {
 
         final EditText note = (EditText) findViewById(R.id.noteText);
 
-        note.setText(MainActivity.noteText);
+        SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+        noteText = sharedPreferences.getString("note", null);
+        if (noteText == null) {
+            note.setText(getString(R.string.note));
+        } else {
+            note.setText(noteText);
+        }
 
         btnSaveNote = findViewById(R.id.saveNote);
         btnSaveNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                MainActivity.noteText = note.getText().toString();
+                noteText = note.getText().toString();
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+
+                sharedPreferences.edit().putString("note", noteText).apply();
             }
         });
 
