70a3b3a679ef57c98c9eda83875e9b9139ac5a11
Reznot <Reznot24@gmail.com>
2019-10-15 16:44:16 +0200

    password protected & password change functionality

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,32 +2,63 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText passwordField;
     private Button btnUnlockNote;
     static String noteText;
+    private String storedPassword;
+    //Obecne haslo to 123
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = getIntent();
+        if (intent.getIntExtra("passwordChanged", -1) == 0){
+            Snackbar snackbarPasswordChanged = Snackbar.make(findViewById(android.R.id.content),"Twoje haslo zostalo zmienione", Snackbar.LENGTH_LONG);
+            View sbView = snackbarPasswordChanged.getView();
+            sbView.setBackgroundColor(Color.GREEN);
+            snackbarPasswordChanged.show();
+        }
+
+        passwordField = findViewById(R.id.passwordField);
         btnUnlockNote = (Button) findViewById(R.id.unlock_button);
 
         btnUnlockNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                launchActivity();
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                storedPassword = sharedPreferences.getString("password", null);
+                if(storedPassword == null) {
+                    sharedPreferences.edit().putString("password", passwordField.getText().toString()).apply();
+                    launchActivity();
+                } else if(storedPassword.equals(passwordField.getText().toString())) {
+                    launchActivity();
+                } else {
+                    Snackbar snackbarWrongPassword = Snackbar.make(view,"Podano bledne haslo", Snackbar.LENGTH_LONG);
+                    View sbView = snackbarWrongPassword.getView();
+                    sbView.setBackgroundColor(Color.RED);
+                    snackbarWrongPassword.show();
+                }
             }
         });
     }
 
     private void launchActivity() {
+        passwordField.setText("");
         Intent intent = new Intent(this, note_screen.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/notes/PasswordChangeActivity.java
+++ app/src/main/java/com/example/notes/PasswordChangeActivity.java
@@ -2,13 +2,56 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class PasswordChangeActivity extends AppCompatActivity {
+
+    private EditText prevPasswwordField;
+    private EditText newPasswordField;
+    private Button btnChangePassword;
+    private String storedPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_change);
+
+        prevPasswwordField = findViewById(R.id.prevPassword);
+        newPasswordField = findViewById(R.id.newPassowrd);
+        btnChangePassword = findViewById(R.id.confirmPasswordChangeBtn);
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
+                storedPassword = sharedPreferences.getString("password", null);
+                if (prevPasswwordField.getText().toString().equals(storedPassword)) {
+                    sharedPreferences.edit().putString("password", newPasswordField.getText().toString()).apply();
+                    passwordChanged();
+                } else {
+                    Snackbar snackbarWrongPassword = Snackbar.make(view,"Podano bledne obecne haslo", Snackbar.LENGTH_LONG);
+                    View sbView = snackbarWrongPassword.getView();
+                    sbView.setBackgroundColor(Color.RED);
+                    snackbarWrongPassword.show();
+                }
+            }
+        });
+    }
+
+    private void passwordChanged() {
+        prevPasswwordField.setText("");
+        newPasswordField.setText("");
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("passwordChanged", 0);
+        startActivity(intent);
     }
 }
